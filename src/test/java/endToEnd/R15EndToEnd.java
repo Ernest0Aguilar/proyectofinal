acc81c0b0d1f55d4d426455ae8a4071cc7bcc662
dev-oswld <oswld@_mail.com>
2021-07-09 09:26:15 -0500

    new driver config

--- src/test/java/endToEnd/R15EndToEnd.java
+++ src/test/java/endToEnd/R15EndToEnd.java
@@ -29,8 +29,7 @@ public class R15EndToEnd {
 	@BeforeTest
 	public void setBaseURL() throws InterruptedException {
 
-		System.setProperty("webdriver.chrome.driver",
-				"C://Users//ernesto.aguilar//Documents//chromedriver_win32//chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",".\\web_drivers_general\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://automationpractice.com/index.php");
 		driver.manage().window().maximize();
